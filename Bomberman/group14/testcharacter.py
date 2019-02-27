685bc9dd68238433fb9b5d1c100ce7f06da623ce
Kyle Richards <kyleLRichards11@gmail.com>
2019-02-26 21:21:24 -0500

    Broken Manhattan Distance

--- Bomberman/group14/testcharacter.py
+++ Bomberman/group14/testcharacter.py
@@ -19,113 +19,8 @@ class TestCharacter(CharacterEntity):
     def do(self, wrld):
         # print("My Location: ", self.x, ", ", self.y)
 
-        # Initialize Policy Matrices
-        policy1 = [[(0, 0, 0) for i in range(wrld.width())] for j in range(wrld.height())]
-        policy2 = [[(0, 0, 0) for i in range(wrld.width())] for j in range(wrld.height())]
-        policies = [policy1, policy2]
-        policyIndex = 1
-
-        nextToWall = False
-
-        # Good ol' Value Iteration?
-
-        # Iterate 50 times
-        for k in range(0, 50):
-            # print("Level", k)
-
-            # For each cell in the world
-            for i in range(0, wrld.width()):
-                for j in range(0, wrld.height()):
-                    max = None
-                    bestA = None
-                    bestB = None
-                    nextToExit = False
-                    deathFound = False
-
-                    # Look at adjacent cells
-                    for a in range(-1, 2):
-                        for b in range(-1, 2):
-
-                            # Ignore the current cell, the exit cell, and cells that are walls
-                            if not (a == 0 and b == 0) and not wrld.exit_at(i, j) and not wrld.wall_at(i, j):
-                                if self.checkInWorldBounds(i+a, j+b, wrld):
-                                    if not wrld.wall_at(i+a, j+b):
-
-                                        # If we are next to the exit, go to the exit
-                                        if wrld.exit_at(i+a, j+b):
-                                            bestA = a
-                                            bestB = b
-                                            max = self.exitReward - 1
-                                            nextToExit = True
-
-                                        # If the bomb will explode soon check if we are near it
-                                        if self.bombTimer is not None and self.bombTimer < 2:
-                                            if self.isInExplodeRange(i, j, wrld):
-                                                max = self.deathReward
-                                                deathFound = True
-
-                                        # Check for explosion cells
-                                        if wrld.explosion_at(i, j):
-                                            max = self.deathReward
-                                            deathFound = True
-
-
-                                        # If we are not next to the exit, find the best cell to go to
-                                        if not nextToExit:
-                                            reward = -1#(wrld.height() - j)
-                                            p = policies[self.getPreviousPolicyIndex(policyIndex)][j + b][i + a]
-                                            v = p[2] + reward
-
-                                            if max is None:
-                                                max = v
-                                                bestA = a
-                                                bestB = b
-                                            else:
-                                                if v > max + reward:
-                                                    if not deathFound:
-                                                        max = v
-                                                    bestA = a
-                                                    bestB = b
-
-                                    # If there is a wall at this spot (from else) and we are at this i, j, then we are next to a wall
-                                    elif i == self.x and j == self.y and (a == 0 or b == 0):
-                                        nextToWall = True
-
-                            # If this is the exit cell, set the reward appropriately
-                            elif wrld.exit_at(i, j):
-                                bestA = 0
-                                bestB = 0
-                                max = self.exitReward
-
-
-                    # Update the policy matrix
-                    policies[policyIndex][j][i] = (bestA, bestB, max)
-
-            # Switch to other policy matrix
-            policyIndex = self.getPreviousPolicyIndex(policyIndex)
-
-        # Print the final policy matrix after 50 iterations
-        print(DataFrame(policies[policyIndex]))
-
-        # If we are near a monster or next to a wall place a bomb
-        if policies[policyIndex][self.y][self.x][2] < self.deathReward - 1 or nextToWall:
-            if self.getBombLocation(wrld)[0] is None:
-                self.place_bomb()
-                self.bombTimer = wrld.bomb_time
-
-        # If there is a bomb, decrease the timer by 1
-        if self.bombTimer is not None:
-            self.bombTimer = self.bombTimer - 1
-
-        print("Bomb Time = ", self.bombTimer)
-        print("Explode Range = ", wrld.expl_range)
-        print("Explode at? = ", wrld.explosion_at(2, 2))
-
-        # Choose best move
-        # print("Im at", self.x, self.y)
-        p = policies[policyIndex][self.y][self.x]
-        # print("My direction is", p[0], p[1])
-        self.move(p[0], p[1])
+        self.getManhattanDist(self.x, self.y, wrld)
+        self.move(0, 0)
 
     def checkForNearbyMonster(self, wrld, i, j, a, b):
         for c in range(-1, 2):
@@ -160,23 +55,59 @@ class TestCharacter(CharacterEntity):
             return True
         return False
 
-    def getPreviousPolicyIndex(self, idx):
-        if idx == 0:
-            return 1
-        else:
-            return 0
 
-
-    def getExitLoc(self, x, y, wrld):
-        if wrld.exit_at(x, y):
-            return x, y
+    def getExitLoc(self, wrld):
+        for i in range(wrld.width()):
+            for j in range(wrld.height()):
+                if wrld.exit_at(i, j):
+                    return i, j
 
     def getEuclideanDist(self, x, y, wrld):
         exit_x, exit_y = self.getExitLoc(x, y, wrld)
         return np.sqrt(((x - exit_x) ^ 2) + ((y - exit_y) ^ 2))
 
     def getManhattanDist(self, x, y, wrld):
-        exit_x, exit_y = self.getExitLoc(x, y, wrld)
-        return np.abs(x-exit_x) + np.abs(y-exit_y)
+
+        h1 = [[1000 for i in range(wrld.width())] for j in range(wrld.height())]
+        h2 = [[1000 for i in range(wrld.width())] for j in range(wrld.height())]
+
+        exit_x, exit_y = self.getExitLoc(wrld)
+
+        h1[exit_y][exit_x] = 0
+        h2[exit_y][exit_x] = 0
+
+        h = [h1, h2]
+
+        h_index = 0
+
+        for k in range(0, 1):
+            for i in range(wrld.width()):
+                for j in range(wrld.height()):
+                    if not wrld.wall_at(i, j) and not wrld.exit_at(i, j):
+                        min = None
+                        for a in range(-1, 2):
+                            for b in range(-1, 2):
+                                if self.checkInWorldBounds(i + a, j + b, wrld):
+                                    if not wrld.wall_at(i + a, j + b):
+                                        v = h[h_index][j][i]
+                                        if min is None:
+                                            min = v
+                                        else:
+                                            if v < min:
+                                                min = v
+
+                        h[self.getOtherIndex(h_index)][j][i] = min + 1
+
+            h_index = self.getOtherIndex(h_index)
+
+        print(DataFrame(h[h_index]))
+
+        return h[h_index][y][x]
+
+    def getOtherIndex(self, idx):
+        if idx == 0:
+            return 1
+        else:
+            return 0
 
 
